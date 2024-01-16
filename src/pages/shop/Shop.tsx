c3e80b97dc0f4da8baf0b477d9e63e877c117c0e
Sunil Park <sunilpark1129@gmail.com>
2024-01-16 12:44:46 -0500

    ADD - added error handling and comments

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -6,39 +6,55 @@ import { AppDispatch, RootState } from "../../redux/store";
 import { MyData } from "../../redux/features/getSlice";
 import ShopAside from "./ShopAside";
 import DisplayItems from "./DisplayItems";
-type Props = {};
 
-type IdProp = { id: string | undefined };
+type ParamProp = { id: string | undefined };
 
-function Shop({}: Props) {
-  const { id }: any = useParams<IdProp>();
+function Shop() {
+  /* param states */
+  const { id }: any = useParams<ParamProp>();
   const [paramCategory, paramGender] = id.split("-");
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(
-    paramCategory
-  );
-  const [selectedGender, setSelectedGender] = useState<string | null>(
-    paramGender
-  );
+  const [wrongParam, setWrongParam] = useState(false);
+
+  /* resources to fetch and filter */
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedGender, setSelectedGender] = useState<string | null>(null);
   const [filteredData, setFilteredData] = useState<MyData[] | null>(null);
 
+  /* request and receive the data from server */
   const dispatch = useDispatch<AppDispatch>();
-
   const { data, loading, error } = useSelector(
     (state: RootState) => state.getPost
   );
 
   useEffect(() => {
-    setSelectedCategory(paramCategory);
-    setSelectedGender(paramGender);
+    const validCategory = ["coat", "shirt", "hoodie", "sweater"];
+    const validGender = ["men", "women"];
+    if (paramCategory) {
+      // used a validated param
+      if (
+        !validCategory.includes(paramCategory) ||
+        !validGender.includes(paramGender)
+      ) {
+        setWrongParam(true);
+      } else {
+        setSelectedCategory(paramCategory);
+        setSelectedGender(paramGender);
+      }
+    }
+    return () => {
+      setWrongParam(false);
+    };
   }, [id]);
 
   useEffect(() => {
-    if (selectedCategory) {
+    // new HTTP request
+    if (selectedCategory && !wrongParam) {
       dispatch(requestHTTP(selectedCategory));
     }
   }, [selectedCategory]);
 
   useEffect(() => {
+    // filtering the received data
     if (selectedCategory && data.length !== 0) {
       const convertGender = selectedGender === "men" ? "male" : "female";
       const temp = data.filter(({ gender }) => gender === convertGender);
@@ -46,7 +62,9 @@ function Shop({}: Props) {
     }
   }, [selectedGender, data]);
 
+  // a component to display current pending stage
   function PendingData() {
+    if (wrongParam) return <WrongParamComponent />;
     if (error) return <ErrorComponent err={error} />;
     if (loading || !filteredData) return <LoadingComponent />;
     return <DisplayItems filteredData={filteredData} />;
@@ -62,7 +80,11 @@ function Shop({}: Props) {
   );
 }
 
-function ErrorComponent(err: any) {
+function WrongParamComponent() {
+  return <div>Wrong Param...</div>;
+}
+
+function ErrorComponent({ err }: any) {
   return <div>{err}</div>;
 }
 
