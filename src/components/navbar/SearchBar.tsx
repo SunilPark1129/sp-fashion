a885dfd04b9b3112ed822b884f36f57392244aa6
Sunil Park <sunilpark1129@gmail.com>
2024-01-24 12:44:19 -0500

    Add - Serachbar svg and event

--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -52,15 +52,30 @@ function SearchBar() {
     }
   }
 
-  /* pass searched term to other route location */
+  function keyDownHandler(e: React.KeyboardEvent) {
+    if (e.key === "Enter") {
+      searchClickHandler();
+    }
+  }
+
   function listClickHandler(item: string) {
     setSearchTerm(item);
     setHasFocus(false);
+    searchClickHandler();
+  }
+
+  /* pass searched term to other route location */
+  function searchClickHandler() {
+    if (searchTerm.trim().length === 0) return;
     navigate("/shop/coat-women");
   }
 
   return (
-    <div className="searchbar" onFocus={() => setHasFocus(true)}>
+    <div
+      className="searchbar"
+      onFocus={() => setHasFocus(true)}
+      onKeyDown={keyDownHandler}
+    >
       <input
         className="searchbar__input"
         type="text"
@@ -69,6 +84,22 @@ function SearchBar() {
         placeholder="Search Clothes"
         autoComplete="off"
       />
+      <div className="searchbar__icon" onClick={searchClickHandler}>
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M14.2122 7.52637C14.1464 8.88311 13.6912 10.1288 12.9599 11.1605L17.3741 15.841L15.3753 17.7261L10.9567 13.0408C9.81642 13.7452 8.45862 14.1239 7.01875 14.054C3.22976 13.8702 0.30722 10.6496 0.491075 6.86057C0.67493 3.07158 3.89556 0.149044 7.68454 0.3329C11.4735 0.516755 14.3961 3.73738 14.2122 7.52637ZM7.15187 11.3098C9.42526 11.4201 11.3576 9.6666 11.468 7.39321C11.5783 5.11982 9.82474 3.18744 7.55135 3.07713C5.27796 2.96681 3.34558 4.72034 3.23527 6.99373C3.12496 9.26712 4.87848 11.1995 7.15187 11.3098Z"
+            fill="black"
+          />
+        </svg>
+      </div>
       {hasFocus && !error && filteredLists.length !== 0 && (
         <ul className="searchbar__lists">
           {filteredLists.map((item) => (
