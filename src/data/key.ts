08ec7bea7a52f11487ec3142e101c71819408102
Sunil Park <sunilpark1129@gmail.com>
2024-01-26 10:10:05 -0500

    FIXED - filled empty color values on json

--- src/data/key.ts
+++ src/data/key.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.npoint.io/0db7e7f0e2db9c9f025c";
+export const BASE_URL = "https://api.npoint.io/ffccefcb164e7373a18c";
 
 export const IMAGE_KEY: string =
   "https://github.com/SunilPark1129/sunilpark1129/assets/106734133/";
