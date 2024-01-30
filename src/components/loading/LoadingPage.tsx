c1254e7c332d4d24a70f2518c3134b5a8a7a4a94
Sunil Park <sunilpark1129@gmail.com>
2024-01-30 14:37:09 -0500

    EDITED - loading style

--- src/components/loading/LoadingPage.tsx
+++ src/components/loading/LoadingPage.tsx
@@ -25,49 +25,70 @@ function LoadingPage() {
           </div>
           <div className="loading__main">
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
               </div>
             </div>
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
               </div>
             </div>
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
               </div>
             </div>
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
               </div>
             </div>
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
               </div>
             </div>
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
               </div>
             </div>
             <div className="loading__main__box">
-              <div className="loading__main__box__text"></div>
+              <div className="loading__main__box__item">
+                <div className="loading__main__box__img"></div>
+                <div className="loading__main__box__text"></div>
+              </div>
               <div className="loading__main__box__btn">
                 <div className="loading__main__box__svg"></div>
                 <div className="loading__main__box__svg"></div>
