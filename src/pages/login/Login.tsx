f20a23e1c2efdf3432f81bf82607aa58aa95b09b
Sunil Park <sunilpark1129@gmail.com>
2024-01-30 21:35:48 -0500

    ADDED - login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,60 @@
-import React from "react";
-
+import React, { useState } from "react";
+import photo from "../home/assets/banner_recycle.jpg";
+import { Link } from "react-router-dom";
+import "./login.css";
 type Props = {};
 
 function Login({}: Props) {
-  return <div>Login</div>;
+  const [hasValidated, setHasValidated] = useState<boolean | null>(null);
+
+  function submitHandler(e: React.FormEvent) {
+    e.preventDefault();
+    setHasValidated(false);
+  }
+
+  return (
+    <section className="login">
+      <div className="wrapper">
+        <div className="container">
+          <div className="login__img">
+            <img src={photo} alt="hoodie and pants" />
+          </div>
+          <div className="login__text">
+            <div className="login__header">
+              <h1>SIGN IN</h1>
+            </div>
+            <form className="login__form" onSubmit={submitHandler}>
+              <div className="login__input-box">
+                <label htmlFor="login">
+                  EMAIL ID:
+                  <input type="email" id="login" autoComplete="off" required />
+                </label>
+                <label htmlFor="password">
+                  PASSWORD:
+                  <input
+                    type="text"
+                    id="password"
+                    autoComplete="off"
+                    required
+                  />
+                </label>
+              </div>
+              <div className="login__validated">
+                {hasValidated === false && <p>Invalid Email or Password</p>}
+              </div>
+              <div className="login__links">
+                <div className="login__btn-box">
+                  <button type="submit">SIGN IN</button>
+                  <button type="button">CREATE NEW</button>
+                </div>
+                <Link to={"/login"}>Forgot password?</Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
