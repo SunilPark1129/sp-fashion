ff935e79d8cbc47d5e94c38e6894d61eabccdcc2
Sunil Park <sunilpark1129@gmail.com>
2024-04-23 09:45:59 -0400

    added keys on array div

--- src/components/loading/LoadingPage.tsx
+++ src/components/loading/LoadingPage.tsx
@@ -24,8 +24,8 @@ function LoadingPage() {
             </div>
           </div>
           <div className="loading__main">
-            {Array.from({ length: 8 }, () => (
-              <div className="loading__main__box">
+            {Array.from({ length: 8 }, (_, idx) => (
+              <div key={idx} className="loading__main__box">
                 <div className="loading__main__box__item">
                   <div className="loading__main__box__img"></div>
                   <div className="loading__main__box__text">
